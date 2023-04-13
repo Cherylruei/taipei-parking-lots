a87acca3aff3af5f3036c5a803f53e9c90c81b29
cherylruei <cherylrueichun@gmail.com>
2023-04-13 18:31:43 +0800

    feat: project init

---
+++ src/api/index.js
@@ -0,0 +1,2 @@
+import axios from "axios";
+// 將停車場的API 用 axios.get 拿到資料
---
+++ src/utils/breakpoint.js
@@ -0,0 +1,5 @@
+// phone under 768px
+// tablet
+export const MEDIA_QUERY_MD = '@media screen and (min-width: 768px)';
+// desktop
+export const MEDIA_QUERY_LG = '@media screen and (min-width: 1024px)';
