a19d0a3868d1518f458030b6468e6ac059300d8c
cherylruei <cherylrueichun@gmail.com>
2023-04-22 17:21:06 +0800

    feat: add available places data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,24 +6,29 @@ const StyledList = styled.div`
   div {
     margin: 16px 5px;
     padding: 5px 16px;
+    line-height: 1.5rem;
     border: 2px solid var(--color-white-grey);
     h2 {
-      font-size: 700;
+      font-weight: 700;
     }
   }
 `;
 
-const List = ({ visibleLots }) => {
+const List = ({ visibleLots, availablePlaces }) => {
   const width = window.innerWidth;
   console.log('width', width);
   return (
     <StyledList>
       {visibleLots.map((parkingLot) => {
+        const availablePlace = availablePlaces.find(
+          (place) => place.id === parkingLot.id
+        );
         return (
           <div key={parkingLot.id}>
             <h2>{parkingLot.name}</h2>
             <p>地址:{parkingLot.address}</p>
             <p>總停車位:{parkingLot.totalcar}</p>
+            <p>剩餘空位:{availablePlace.availablecar}</p>
           </div>
         );
       })}
@@ -31,11 +36,11 @@ const List = ({ visibleLots }) => {
   );
 };
 
-const Navbar = ({ setCoords, visibleLots }) => {
+const Navbar = ({ setCoords, visibleLots, availablePlaces }) => {
   return (
     <>
       <SearchInput setCoords={setCoords} />
-      <List visibleLots={visibleLots} />
+      <List visibleLots={visibleLots} availablePlaces={availablePlaces} />
     </>
   );
 };
